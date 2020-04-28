d95e171b6739ec8065049f93f9435130098b037f
github username <github email address>
2020-04-28 22:04:18 +0100

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  #
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    passing_triangle?
+    if a == b && b == c
+      :equilateral
+    elsif a == c || a == b || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def passing_triangle?
+    passing_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    raise TriangleError if passing_triangle.include?(false)
+  end
+
+
+  class TriangleError < StandardError
+
+  end
+
+
 end
